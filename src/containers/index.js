a0c7ebf7976e959e91477d3af428cc43599264b1
Sam Gershuny <samgershuny@Lawrences-MacBook-Pro-4.local>
2021-09-30 09:54:26 +0100

    Remove statechain & Coin

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,9 +2,8 @@ export { default as TopNavigation } from './TopNavigation';
 
 export { default as Home } from './Home';
 
-export { default as Statechain } from './Statechain';
+
 export { default as ScAddress } from './ScAddress';
-export { default as Coin } from './Coin';
 export { default as Swap } from './Swap';
 export { default as Transaction } from './Transaction'
 
