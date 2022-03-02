3697706e02b71a5a4cbe559a09d4ef4b00a3f7d4
Sam Gershuny <samgershuny@Lawrences-MacBook-Pro-4.local>
2022-03-02 17:04:01 +0000

    remove self signing

--- api/server.js
+++ api/server.js
@@ -3,13 +3,6 @@ const cors = require("cors");
 
 const app = express();
 
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
 
 var corsOptions = {
   origin: "http://localhost:3000"
@@ -39,5 +32,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-https.createServer(options, app).listen(443);
