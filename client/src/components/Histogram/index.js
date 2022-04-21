002587c951c78a1c1215769e743109940b846cd4
Sam Gershuny <samgershuny@Lawrences-MacBook-Pro-4.local>
2022-04-21 15:21:55 +0100

    #32 No Bar No Liquidity

--- client/src/components/Histogram/index.js
+++ client/src/components/Histogram/index.js
@@ -11,22 +11,34 @@ const Histogram = (props) => {
     const [data,setData] = useState()
     const withdrawHistoData = useSelector(withdrHistoSelector)
     const depositHistoData = useSelector(depHistoSelector)
+    // All deposited coins
     const depositHistoStatus = useSelector(depHistoStatus)
     const withdrawHistoStatus = useSelector(withdrHistoStatus)
+    // All withdrawn coins
 
     useEffect(()=> {
         let withdrawCount = 0
         let histogram = []
         let swapAmounts = [100000,500000,1000000,5000000,10000000,50000000,100000000]
+        //x values to show on histogram coin liquidity graph: most commonly used swap values
+
         if(depositHistoStatus === "fulfilled" && withdrawHistoStatus === "fulfilled"){
+            
             depositHistoData.map(item => {
                 let withdrawTotal = withdrawHistoData.filter(value => value._id === item._id)
+                // Get total withdrawn coins for value of item in depositHistoData
 
                 if(swapAmounts.includes(item._id) && withdrawTotal[0]){
-                    histogram.push({value: item._id, count: (item.count - withdrawTotal[0].count)})
+                    let currentLiquidity = item.count - withdrawTotal[0].count
+                    if(currentLiquidity !== 0){
+                    // If no current liquidity: don't show in graph
+                        histogram.push({value: item._id, count: (item.count - withdrawTotal[0].count)})
+                        // total deposited - total withdrawn = current liquidity
+                    }
                 }
                 else if(swapAmounts.includes(item._id) ){
                     histogram.push({value: item._id, count: (item.count)})
+                    // if coin value has never been withdrawn before then total deposited = current liquidity
                 }
             })
         }
