c2ff518c2863c4f98c97ce36627ffe7012265124
Sam Gershuny <samgershuny@Lawrences-MacBook-Pro-4.local>
2022-05-06 18:12:21 +0100

    CSV summary api

--- api/server.js
+++ api/server.js
@@ -40,7 +40,7 @@ app.get("/csv", csv.getSummary);
 app.get("/testnet/csv", csv_testnet.getSummary);
 
 
-var whitelist = [ "https://testnet-mercury-explorer.netlify.app", "https://explorer.mercurywallet.com", "https://testnet.explorer.mercurywallet.com", "http://localhost:3000"]
+var whitelist = [ "https://testnet-mercury-explorer.netlify.app", "https://explorer.mercurywallet.com", "https://testnet.explorer.mercurywallet.com"]
 
 var corsOptions = {
   origin: function (origin, callback) {
